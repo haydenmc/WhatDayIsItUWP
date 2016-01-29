dd3038480dac1190535f6d796da7e0a5f9aae805
Hayden McAfee <hamc@microsoft.com>
2016-01-29 15:13:52 -0800

    It works!

---
+++ MyAwesomeApp/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MyAwesomeApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        private DateTimeOffset selectedDate;
+        public DateTimeOffset SelectedDate
+        {
+            get
+            {
+                return selectedDate;
+            }
+            set
+            {
+                if (selectedDate != value)
+                {
+                    selectedDate = value;
+                    OnPropertyChanged("SelectedDate");
+                    OnPropertyChanged("DayOfWeek");
+                }
+            }
+        }
+        public string DayOfWeek
+        {
+            get
+            {
+                if (SelectedDate != null)
+                {
+                    return SelectedDate.DayOfWeek.ToString();
+                } else
+                {
+                    return "";
+                }
+            }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SelectedDate = DateTimeOffset.Now;
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
